5bf43bba5d4334cbd6176a7e367780b2b4628c06
Bijay Jha <bijayjha12345@gmail.com>
2026-02-03 19:49:53 +0545

    Project setup and installation

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import { LayoutDashboard } from 'lucide-react'
+function App() {
+  
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+     <LayoutDashboard size={48} className="mb-4 text-blue-500" />
+     <p>It is an admin dashboard under working. This is project setup and installation of required stuffs like React, Vite, TailwindCSS, Lucide icons, react-router-dom, recharts</p>
+    </div>
+  )
+}
+
+export default App
